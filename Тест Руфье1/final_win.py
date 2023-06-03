3309e99ccfbf489982f0315662c081a63b74968c
Akim112 <135325901+Akim112@users.noreply.github.com>
2023-06-03 18:05:03 +0300

    Add files via upload

---
+++ Тест Руфье1/final_win.py
@@ -0,0 +1,22 @@
+#final_win.py
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLabel,QWidget, QApplication,QPushButton,QVBoxLayout,QHBoxLayout,QLineEdit
+from instr import *
+
+class FinalWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(txt_finalwin)
+        self.resize(win_width,win_height)
+        self.move(win_x,win_y)
+    def initUI(self):
+        self.line = QVBoxLayout()
+        self.txt_index = QLabel(txt_index)
+        self.txt_workheart = QLabel(txt_workheart)
+        self.line.addWidget(self.txt_index,alignment=Qt.AlignCenter)
+        self.line.addWidget(self.txt_workheart,alignment=Qt.AlignCenter)
+        self.setLayout(self.line)
